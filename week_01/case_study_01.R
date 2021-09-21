b5e6a6b2bb1fa3a4ddf155fba8ce82b6eb1f7b0b
Alex Gould <ajgould@buffalo.edu>
2021-09-21 10:03:22 -0400

    Finished first case study!

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,30 @@
+
+#Pull Library
+library(ggplot2)
+
+#Load Data
 data(iris)
 
+#Pull petal length from table
+iris$Petal.Length
+
+#Save the mean as an object
+petal_legnth_mean <- mean(iris$Petal.Length)
+
+#Calculate the mean
+petal_legnth_mean
+
+#Create ggplot Histogram
+qplot(iris$Petal.Length,geom="histogram")
+
+#adjustments
+qplot(iris$Petal.Length,
+      geom="histogram",
+      binwidth = .5,  
+      main = "Histogram for Petal Length", 
+      xlab = "Length in cm",
+      ylab= "Count",
+      fill= (iris$Species),
+      col=I("yellow"))
+
+#I treats the thing as a command eg. fill= I("pink"), would put everything as pink
