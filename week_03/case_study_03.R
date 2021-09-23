586e2f7986fc27dce32156cddd8a46e81294ed03
Alex Gould <ajgould@buffalo.edu>
2021-09-22 21:16:35 -0400

    Case Study 3

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,1 +1,42 @@
- 
+#load ggplot 2
+library(ggplot2)
+
+#load gapminder
+ library(gapminder)
+
+#load dplyr
+ library(dplyr) 
+
+#view gapminder data
+gapminder
+
+#remove Kuwait from gapminder dataset 
+filter(gapminder, country!="Kuwait")
+
+#save it!
+No_Kuwait <- filter(gapminder, country!="Kuwait")
+
+#plot it!
+p1 <- ggplot(No_Kuwait, aes(x = lifeExp, y =gdpPercap, color = continent, size = pop/100000))  + 
+    geom_point() +
+    scale_y_continuous(trans= "sqrt")+ 
+     labs(x = "Life Expectancy", y ="GDP Per Capita", size = .5, color = "#00008") + 
+     facet_wrap(~year, nrow=1) +
+     theme_bw()
+
+p1   
+
+#get ready for the second plot
+
+#group continent and year and save as grouping
+grouping <-gapminder %>% group_by(continent, year)
+
+#run it 
+grouping     
+
+#summarize it ans save it as gapminder_continent
+gapminder_continent <- summarise(gdpPercapweighted= weighted.mean(x=gdpPercap, w=pop)) +
+  pop = sum(as.numeric(pop))
+
+#plot it
+ggplot(gapminder_continent)
