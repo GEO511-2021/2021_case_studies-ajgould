1fbd20c5e8f657708a041ebcd955fac2bc4e9070
Alex Gould <ajgould@buffalo.edu>
2021-09-22 22:09:40 -0400

    What is going on?

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -20,7 +20,7 @@ No_Kuwait <- filter(gapminder, country!="Kuwait")
 p1 <- ggplot(No_Kuwait, aes(x = lifeExp, y =gdpPercap, color = continent, size = pop/100000))  + 
     geom_point() +
     scale_y_continuous(trans= "sqrt")+ 
-     labs(x = "Life Expectancy", y ="GDP Per Capita", size = .5, color = "#00008") + 
+     labs(x = "Life Expectancy", y ="GDP Per Capita", size = "Population in 100k", color = "Continent") + 
      facet_wrap(~year, nrow=1) +
      theme_bw()
 
@@ -28,15 +28,37 @@ p1
 
 #get ready for the second plot
 
-#group continent and year and save as grouping
-grouping <-gapminder %>% group_by(continent, year)
-
-#run it 
-grouping     
-
-#summarize it ans save it as gapminder_continent
-gapminder_continent <- summarise(gdpPercapweighted= weighted.mean(x=gdpPercap, w=pop)) +
-  pop = sum(as.numeric(pop))
+#group and summarize continent and year and save as grouping
+gapminder_continent <- gapminder %>%
+  group_by(continent, year) %>%
+  summarize(gdpPercapweighted= weighted.mean(x=gdpPercap, w=pop, pop=sum(as.numeric(pop)))) 
+ 
 
 #plot it
-ggplot(gapminder_continent)
+gapminder 
+p2 <- ggplot(gapminder, aes(x = year, y =gdpPercap, color=continent, size=pop/100000)) +
+  geom_line(aes(group=country, size = .1)) + 
+  geom_point() +
+  geom_line(gapminder_continent, mapping=aes(x=year, y=gdpPercapweighted, color= "black", size=.5))+
+  geom_point(gapminder_continent,mapping=aes(x=year, y=gdpPercapweighted, color="black"))+
+  facet_wrap(~continent,nrow=1) + 
+  theme_bw() + 
+  labs( x="Year", y = "GDP per Capita", size =" Population (100k)")
+
+#p2
+
+#Joe's... still saying cant find pop or country 
+# ggplot(gapminder, aes(x = year, y = gdpPercap))+
+ # geom_line(aes(color=continent, group=country))+
+ # geom_point(aes(color=continent))+
+ # geom_line(gapminder_continent, mapping = aes(year, gdpPercapweighted))+
+  #geom_point(gapminder_continent, mapping = aes(year, gdpPercapweighted, size = pop))+
+ # theme_bw()+
+ # facet_wrap(~continent,nrow=1)+
+  #labs(x = "Year", y = "GDP per Capita")
+
+
+
+ 
+
+
